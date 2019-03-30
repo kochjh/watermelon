be8fdc764a4ee4d85dc92fbf5d0a9d2f5bcf17a9
Jan Koch <kochj@uni-bremen.de>
2019-03-30 21:16:29 +0100

    kleine changes

--- src/qualityAndSteps.py
+++ src/qualityAndSteps.py
@@ -10,11 +10,11 @@ with open('../Data/sleepdata.csv', newline='') as f:
 	for x in l:
 		x[2] = int(x[2][:-1])#delete percentage char and parse into int
 		x[7] = float(x[7])
-		if x[7] > 0:
+		if x[7] > 0:#filter steps > 0
 			sq.append(x[2])
 			steps.append(x[7])
 	plt.plot(sq, steps, 'bs')
-	plt.ylabel('quality of sleep in %')
-	plt.xlabel('')
+	plt.ylabel('activity steps')
+	plt.xlabel('quality of sleep in %')
 	plt.show()
 
