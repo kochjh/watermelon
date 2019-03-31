7ee7c56dbb754c80a5849b68b8721e3b39aabbef
Jan Koch <kochj@uni-bremen.de>
2019-03-31 10:39:41 +0200

    Bilder

--- src/sleepingtimeAndQuality.py
+++ src/sleepingtimeAndQuality.py
@@ -2,10 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-def durationAsInt(duration):
-	h = int(duration[:2])
-	min = int(duration[3:5])
-	return h * 60 + min
+def qualityAsInt(quality):
+	return int(quality[:-1])
 
 def getStartingHour(row):
 	return int(row[1][:2])
@@ -22,53 +20,53 @@ with open('../Data/sleepdata_weekday_correct.csv', newline='') as f:
 		if int(x[6][:2]) > 3:
 			if x[2] != "Freitag" and x[2] != "Samstag":
 				if getStartingHour(x) == 20:
-					days[0] += durationAsInt(x[6])
+					days[0] += qualityAsInt(x[5])
 					counter[0] += 1
 				elif getStartingHour(x) == 21:
-					days[1] += durationAsInt(x[6])
+					days[1] += qualityAsInt(x[5])
 					counter[1] += 1
 				elif getStartingHour(x) == 22:
-					days[2] += durationAsInt(x[6])
+					days[2] += qualityAsInt(x[5])
 					counter[2] += 1
 				elif getStartingHour(x) == 23:
-					days[3] += durationAsInt(x[6])
+					days[3] += qualityAsInt(x[5])
 					counter[3] += 1
 				elif getStartingHour(x) == 0:
-					days[4] += durationAsInt(x[6])
+					days[4] += qualityAsInt(x[5])
 					counter[4] += 1
 				elif getStartingHour(x) == 1:
-					days[5] += durationAsInt(x[6])
+					days[5] += qualityAsInt(x[5])
 					counter[5] += 1
 				elif getStartingHour(x) == 2:
-					days[6] += durationAsInt(x[6])
+					days[6] += qualityAsInt(x[5])
 					counter[6] += 1
 				else:
-					days[7] += durationAsInt(x[6])
+					days[7] += qualityAsInt(x[5])
 					counter[7] += 1
 			else:
 				if getStartingHour(x) == 20:
-					days2[0] += durationAsInt(x[6])
+					days2[0] += qualityAsInt(x[5])
 					counter2[0] += 1
 				elif getStartingHour(x) == 21:
-					days2[1] += durationAsInt(x[6])
+					days2[1] += qualityAsInt(x[5])
 					counter2[1] += 1
 				elif getStartingHour(x) == 22:
-					days2[2] += durationAsInt(x[6])
+					days2[2] += qualityAsInt(x[5])
 					counter2[2] += 1
 				elif getStartingHour(x) == 23:
-					days2[3] += durationAsInt(x[6])
+					days2[3] += qualityAsInt(x[5])
 					counter2[3] += 1
 				elif getStartingHour(x) == 0:
-					days2[4] += durationAsInt(x[6])
+					days2[4] += qualityAsInt(x[5])
 					counter2[4] += 1
 				elif getStartingHour(x) == 1:
-					days2[5] += durationAsInt(x[6])
+					days2[5] += qualityAsInt(x[5])
 					counter2[5] += 1
 				elif getStartingHour(x) == 2:
-					days2[6] += durationAsInt(x[6])
+					days2[6] += qualityAsInt(x[5])
 					counter2[6] += 1
 				else:
-					days2[7] += durationAsInt(x[6])
+					days2[7] += qualityAsInt(x[5])
 					counter2[7] += 1
 	print(days)
 	days[0] /= counter[0]
@@ -77,13 +75,13 @@ with open('../Data/sleepdata_weekday_correct.csv', newline='') as f:
 	days[3] /= counter[3]
 	days[4] /= counter[4]
 	days[5] /= counter[5]
-	if days[6] != 0:
+	if counter[6] != 0:
 		days[6] /= counter[6]
-	if days[7] != 0:
+	if counter[7] != 0:
 		days[7] /= counter[7]
 	print(days)
 	
-	if days[6] != 0:
+	if counter2[0] != 0:
 		days2[0] /= counter2[0]
 	days2[1] /= counter2[1]
 	days2[2] /= counter2[2]
@@ -112,7 +110,7 @@ with open('../Data/sleepdata_weekday_correct.csv', newline='') as f:
 	label='weekend')
 	 
 	plt.xlabel('starttime of sleep')
-	plt.ylabel('avg. sleep duration in min')
+	plt.ylabel('avg. sleep quality in %')
 	plt.xticks(index + bar_width / 2, ('20:00-20:59', '21:00-21:59', '22:00-22:59', '23:00-23:59', '00:00-00:59', '01:00-01:59', '02:00-02:59', '03:00-19:59'))
 	plt.legend()
 	 
